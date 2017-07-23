768df06c0a45947b9a3816df29c2212e14bddf9e
Grant Best <grantbst@gmail.com>
2017-07-22 20:13:24 -0700

    Layout finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,40 @@
 
-// get the model
-var modal = document.getElementById('myModal');
-
-// get the image that opens the modal
+// Mean Topics Modal
+var topicModal = document.getElementById('myModal');
 var btn = document.getElementById('openTopic');
-
-// get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
+var dockitModal = document.getElementById('dockitModal');
+var dockBtn = document.getElementById('openDockit');
+var spanDock = document.getElementsByClassName("dockClose")[0];
 // user clicks image to open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  topicModal.style.display = "block";
 }
 
 // user click span to close the model
 span.onclick = function() {
-  modal.style.display = "none";
+  topicModal.style.display = "none";
+
 }
 
 // click anywhere outside the modal closes it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == topicModal) {
+    topicModal.style.display = "none";
   }
+  if (event.target == dockitModal) {
+    dockitModal.style.display = "none";
+  }
+}
+
+// Dockit Modal
+dockBtn.onclick = function() {
+  dockitModal.style.display = "block";
+}
+
+spanDock.onclick = function() {
+  dockitModal.style.display = "none";
+}
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
 }
